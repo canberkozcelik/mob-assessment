ab502f03b86416c00c80dc15ac580734b39f3656
Canberk Özçelik <canberk.ozcelik@gmail.com>
2020-11-08 22:58:30 +0300

    fix json converting, remove client layer

--- app/src/main/java/com/mobiquity/assessment/network/MobServiceClient.kt
+++
@@ -1,8 +0,0 @@
-package com.mobiquity.assessment.network
-
-import javax.inject.Inject
-
-class MobServiceClient @Inject constructor(private val mobService: MobService) {
-
-    suspend fun getCategories() = mobService.getCategories()
-}
---
+++ app/src/main/java/com/mobiquity/assessment/network/data/CategoryResponse.kt
@@ -0,0 +1,49 @@
+package com.mobiquity.assessment.network.data
+
+import android.annotation.SuppressLint
+import android.os.Parcelable
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+import kotlinx.android.parcel.Parcelize
+
+@SuppressLint("ParcelCreator")
+@Parcelize
+@JsonClass(generateAdapter = true)
+data class Category(
+    @Json(name = "description")
+    val description: String?,
+    @Json(name = "id")
+    val id: String?,
+    @Json(name = "name")
+    val name: String?,
+    @Json(name = "products")
+    val products: List<Product?>?
+) : Parcelable {
+    @SuppressLint("ParcelCreator")
+    @Parcelize
+    @JsonClass(generateAdapter = true)
+    data class Product(
+        @Json(name = "categoryId")
+        val categoryId: String?,
+        @Json(name = "description")
+        val description: String?,
+        @Json(name = "id")
+        val id: String?,
+        @Json(name = "name")
+        val name: String?,
+        @Json(name = "salePrice")
+        val salePrice: SalePrice?,
+        @Json(name = "url")
+        val url: String?
+    ) : Parcelable {
+        @SuppressLint("ParcelCreator")
+        @Parcelize
+        @JsonClass(generateAdapter = true)
+        data class SalePrice(
+            @Json(name = "amount")
+            val amount: String?,
+            @Json(name = "currency")
+            val currency: String?
+        ) : Parcelable
+    }
+}
